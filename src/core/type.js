7daf8a77e14a440876f3069727ca3aef4c2990d4
cenfun <cenfun@gmail.com>
2018-09-30 14:31:46 +0800

    add type

---
+++ src/core/type.js
@@ -0,0 +1,51 @@
+function type(obj) {
+    return typeof obj !== "object" ? typeof obj : Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
+}
+
+function isUndefined(obj) {
+    return obj === void 0;
+}
+
+function isNull(obj) {
+    return obj === null;
+}
+
+function isBoolean(obj) {
+    return typeof (obj) === 'boolean';
+}
+
+function isNumber(obj) {
+    return typeof (obj) === 'number';
+}
+
+function isString(obj) {
+    return typeof (obj) === 'string';
+}
+
+function isObject(obj) {
+    return Object.prototype.toString.call(obj) === '[object Object]';
+}
+
+function isArray(obj) {
+    return Array.isArray ? Array.isArray(obj) : Object.prototype.toString.call(obj) === '[object Array]';
+}
+
+function isFunction(obj) {
+    return typeof (obj) === 'function';
+}
+
+function isDate(obj) {
+    return Object.prototype.toString.call(obj) === '[object Date]';
+}
+
+function isRegExp(obj) {
+    return Object.prototype.toString.call(obj) === '[object RegExp]';
+}
+
+function isError(obj) {
+    return Object.prototype.toString.call(obj) === '[object Error]';
+}
+
+function isArguments(obj) {
+    return Object.prototype.toString.call(obj) === '[object Arguments]';
+}
